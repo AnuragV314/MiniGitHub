cdd67c412f5cc6eb928eaa35f1d1d6f4fe24ca95
AnuragV314 <vermagup01@gmail.com>
2024-02-20 16:13:33 +0530

    MERN-Stack-Project

---
+++ frontend/src/components/Repos.jsx
@@ -0,0 +1,22 @@
+import Repo from "./Repo";
+
+function Repos({ repos, alwaysFullWidth = false }) {
+  const className = alwaysFullWidth ? "w-full" : "lg:w-2/3 w-full";
+
+  return (
+    <div className={`${className} rounded-lg px-8 py-6 backdrop-blur-sm border-r  border-gray-500 hover:bg-gray-600/10`}>
+      <ol className="relative border-s border-gray-200">
+        {repos.map((repo) => (
+          <Repo key={repo.id} repo={repo} />
+        ))}
+        {repos.length === 0 && (
+          <p className="flex items-center justify-center h-32 ">
+            No repos found
+          </p>
+        )}
+      </ol>
+    </div>
+  );
+}
+
+export default Repos;
